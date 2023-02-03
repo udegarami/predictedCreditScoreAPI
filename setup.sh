e076cc244958128ac1d426e5cc6943ce65a4f9b9
Marco Giangreco <giangreco.marco@gmx.ch>
2023-02-03 17:50:06 +0100

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
-mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+#!/bin/sh
+
+apt-get update
+apt-get install -y python3.9
+
+pip install -r requirements.txt
