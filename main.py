c4dd29ead241aa9c49ea214cf265b300e824e4db
Marco Giangreco <giangreco.marco@gmx.ch>
2023-02-07 17:11:35 +0100

    bugfix

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost:8000",
-#     "https://predictedcreditscore.herokuapp.com/",
-# ]
+origins = [
+    "http://localhost:8501/",
+    "https://predictedcreditscore.herokuapp.com/",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://predictedcreditscore.herokuapp.com/"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
